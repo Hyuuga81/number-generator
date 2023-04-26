c9f1ca29d4d6a3c823bfee85875349274ad6be72
Jay <j.adeyina@gmail.com>
2023-04-27 07:40:31 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RandomApp extends Component {
+  state = { value: 0 };
+
+  getRandomNumber = () => Math.floor(Math.random() * this.props.max);
+  HandleClick = () => {
+    this.setState({ value: this.getRandomNumber() });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Stateful Component Test</h1>
+        <h2>{this.state.value}</h2>
+        <button onClick={this.HandleClick}>Get Random Number</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default RandomApp;
